12d61a266ef9ddc829ad93328fe31acb1381a77e
liber87 <bd87@bk.ru>
2020-06-03 22:11:58 +0300

    add stars

--- assets/tvs/multitv/configs/comments.config.inc.php
+++ assets/tvs/multitv/configs/comments.config.inc.php
@@ -17,6 +17,10 @@
 	'caption' => 'Дата',
 	'type' => 'date'
     ),
+	'rate' => array(
+	'caption' => 'Оценка',
+	'type' => 'number'
+    ),
 	'visible' => array(
 	'caption' => 'Показывать',
 	'type' => 'checkbox',
@@ -24,12 +28,68 @@
     ),
 	);
 	$settings['templates'] = array(
-    'outerTpl' => '<div class="feedback_wrap">[+wrapper+]</div>',
+    'outerTpl' => '<div class="comments"><ul class="list_none comment_list mt-4">[+wrapper+]</ul></div>
+	<style>
+	.rating-area {
+	overflow: hidden;
+	width: 153px;	
+	}
+	.rating-area:not(:checked) > input {
+		display: none;
+	}
+	.rating-area:not(:checked) > label, .once_star {
+		float: right;
+		width: 30px;
+		padding: 0;
+		cursor: pointer;
+		font-size: 30px;
+		line-height: 30px;
+		color: lightgrey;
+		text-shadow: 1px 1px #bbb;
+	}
+	.once_star {float:left;}
+	.rating-area:not(:checked) > label:before, .once_star:before {
+		content: \'★\';
+	}
+	.rating-area > input:checked ~ label {
+		color: gold;
+		text-shadow: 1px 1px #c60;
+	}
+	.rating-area:not(:checked) > label:hover,
+	.rating-area:not(:checked) > label:hover ~ label, .once_star.active {
+		color: gold;
+	}
+	.rating-area > input:checked + label:hover,
+	.rating-area > input:checked + label:hover ~ label,
+	.rating-area > input:checked ~ label:hover,
+	.rating-area > input:checked ~ label:hover ~ label,
+	.rating-area > label:hover ~ input:checked ~ label,.once_star.active {
+		color: gold;
+		text-shadow: 1px 1px goldenrod;
+	}
+	.rate-area > label:active {
+		position: relative;
+	}
+	</style>
+	',
     'rowTpl' => '[[if? &is=`[+visible+]:is:1` &then=`
-	<div class="row">
-	<div class="col-xs-12" style="margin-bottom:20px; padding-bottom:20px; border-bottom:1px solid #39C7BB;">
-		<div class="name"><b>[+name+]</b> ([+date:date=`%d.%m.%Y %H:%M`+])</div>
-		<div class="comment">[+comment+]</div>
+	<div class="row">		
+		<div class="comment_block">
+			<div class="rates">
+				<span class="once_star [[if? &is=`[+rate+]:gte:1` &then=`active`]]"></span>
+				<span class="once_star [[if? &is=`[+rate+]:gte:2` &then=`active`]]"></span>
+				<span class="once_star [[if? &is=`[+rate+]:gte:3` &then=`active`]]"></span>
+				<span class="once_star [[if? &is=`[+rate+]:gte:4` &then=`active`]]"></span>
+				<span class="once_star [[if? &is=`[+rate+]:gte:5` &then=`active`]]"></span>
+			</div>
+			<div style="clear:both;"></div>
+			<p class="customer_meta">
+				<span class="review_author">[+name+]</span>
+				<span class="comment-date">[+date:date=`%d.%m.%Y %H:%M`+]</span>
+			</p>
+			<div class="description">
+				<p>[+comment+]</p>
+			</div>
 		</div>
 	</div>
 	`]]'
